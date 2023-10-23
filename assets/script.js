c80f2c1e4fdafae4d06bd97c0350ba76d5c1c691
kemmily Souza <kemmilyps1@gmail.com>
2023-10-23 10:06:49 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Função para carregar e inserir conteúdo HTML em uma seção específica
+function loadAndInsertContent(url, targetElementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(targetElementId).innerHTML = data;
+        });
+}
+
+// Carregar o conteúdo do cabeçalho (header.html) e inserir na seção "header"
+loadAndInsertContent('pages/header.html', 'header');
+
+// Carregar o conteúdo da seção de conteúdo (content.html) e inserir na seção "content"
+loadAndInsertContent('pages/content.html', 'content');
+
+// Carregar o conteúdo do rodapé (footer.html) e inserir na seção "footer"
+loadAndInsertContent('pages/footer.html', 'footer');
